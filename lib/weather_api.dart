dbc14a5c60ce7a9a254d0b000cb3286b13d74e4b
Yusif Hasanzade <87422942+iamyusif@users.noreply.github.com>
2023-02-07 10:20:23 +0400

    Update weather_api.dart

--- lib/weather_api.dart
+++ lib/weather_api.dart
@@ -6,7 +6,7 @@ import 'package:api_weather/havaEsas.dart';
 class WeatherApiCilents {
 
   Future<Weather>havaOyren(String? yer) async {
-    var url = Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=$yer&lang=az&appid=759936c58eaf54de94d044e95c8bea4f&units=metric");
+    var url = Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=$yer&lang=az&appid={you api here}&units=metric");
     var response = await http.get(url);
     var json = jsonDecode(response.body);
     return Weather.fromJson(json);
